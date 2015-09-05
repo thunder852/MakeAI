1cbc56ed7ffa9a3c8b4cd816a220b4d9af0f367d
bevan <thunder852@hotmail.com>
2015-09-05 20:14:22 +0200

    Start
    Imported code

---
+++ GameCode/Assets/Scripts/Game Dashboard/GameParameters.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class GameParameters : MonoBehaviour
+{
+	//Variables for Unity Inspector
+	public int numberOfPlayers=2;
+	public int numberOfBlocksX=100;
+	public int numberOfBlocksY=100;
+	public int startOfMapEdgeOffset=10;
+
+	//Static Variables to be used by scripts
+	public static int numberPlayers;
+	public static int numberBlocksX;
+	public static int numberBlocksY;
+	public static int startMapEdgeOffset;
+
+	//Set static variables
+	void Awake ()
+	{
+		numberPlayers = numberOfPlayers;
+		numberBlocksX = numberOfBlocksX;
+		numberBlocksY = numberOfBlocksY;
+		startMapEdgeOffset = startOfMapEdgeOffset;
+	}
+
+}
---
+++ GameCode/Assets/Scripts/World.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class World : MonoBehaviour {
+
+	public List<Tank>[]Tanks;
+	public List<SpawnTower>[]SpawnTowers;
+	public List<ExtractionTower>[]ExtractionTowers;
+	TerrainCoordinatesAndContent WorldGrid = new TerrainCoordinatesAndContent();
+	GameObject[] unitySpawnTowerObject = new GameObject[2];
+	// Use this for initialization
+	void Start () 
+	{
+		Tanks=new List<Tank>[GameParameters.numberPlayers];
+		SpawnTowers=new List<SpawnTower>[GameParameters.numberPlayers];
+		ExtractionTowers=new List<ExtractionTower>[GameParameters.numberPlayers];
+
+		for (int i=0; i < GameParameters.numberPlayers; i++) 
+		{
+			Tanks [i] = new List<Tank> ();
+			SpawnTowers [i] = new List<SpawnTower> ();
+			ExtractionTowers [i] = new List<ExtractionTower> ();
+			
+			// Spawn Towers
+			int multi = i == 0 ? -1 : 1;
+			SpawnTowers [i].Add (new SpawnTower (100, 100, new Vector2 (i * GameParameters.numberBlocksX - multi * GameParameters.startMapEdgeOffset, i * GameParameters.numberBlocksY - multi * GameParameters.startMapEdgeOffset)));
+			unitySpawnTowerObject [i] = (GameObject)Instantiate (Resources.Load ("Buildings/SimpleBuilding"));
+			unitySpawnTowerObject [i].transform.position = WorldGrid.BlockToTerrainCoordinate (SpawnTowers [i] [0].Location, 5);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+}
